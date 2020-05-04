4f768cfdd3b6880d4db5d14a17cb16e3b079687f
Azzouz <mohamed.azzouz@laplateforme.io>
2020-05-04 18:59:34 +0200

    job04

---
+++ jour01/job04/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Job04</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<header id="header">
+		<a href="#nav-header" id="button"></a>
+		<nav id="nav-header">
+			<ul id="nav-ul">
+				<li><a href="#" class="lien-nav">Accueil</a></li>
+				<li><a href="#" class="lien-nav">Présentation</a></li>
+				<li><a href="#" class="lien-nav">Contact</a></li>
+			</ul>
+		</nav>  
+	</header>
+ 
+	<main>
+		<section class="section">
+
+		</section>
+		<section class="section">
+
+		</section>
+		<section class="section">
+
+		</section>
+		<section class="section">
+
+		</section>
+	</main>
+
+	
+
+</body>
+</html>
