931f62b826a785071dfd7f84886e121d852158f7
Azzouz <mohamed.azzouz@laplateforme.io>
2020-05-04 15:14:52 +0200

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JOB01</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style1.css">
+	<link rel="stylesheet" type="text/css" href="style2.css">
+	<link rel="stylesheet" type="text/css" href="style3.css">
+	<link rel="stylesheet" type="text/css" href="style4.css">
+</head>
+<body>
+
+</body>
+</html>
