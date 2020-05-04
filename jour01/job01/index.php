6d4c37df97c2f72d0a3a405cab03b51cc129efc3
Azzouz <mohamed.azzouz@laplateforme.io>
2020-05-04 15:28:12 +0200

    job01

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -2,11 +2,10 @@
 <html>
 <head>
 	<title>JOB01</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="style1.css">
-	<link rel="stylesheet" type="text/css" href="style2.css">
-	<link rel="stylesheet" type="text/css" href="style3.css">
-	<link rel="stylesheet" type="text/css" href="style4.css">
+	<link rel="stylesheet" type="text/css" media="screen" href="style1.css">
+    <link rel="stylesheet" type="text/css" media="screen and (max-width:1920px)" href="style2.css" >
+    <link rel="stylesheet" type="text/css" media="screen and (max-width:1680px)" href="style3.css" >
+    <link rel="stylesheet" type="text/css" media="screen and (max-width:1280px)" href="style4.css" >
 </head>
 <body>
 
