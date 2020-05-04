c54cc12f2522c2d5878ce63e54e80d8b7be07a34
Azzouz <mohamed.azzouz@laplateforme.io>
2020-05-04 16:40:50 +0200

    job03

---
+++ jour01/job03/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Job03</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<section class="section">
+		
+	</section>
+	<section class="section">
+		
+	</section>
+	<section class="section">
+		
+	</section>
+	<section class="section">
+		
+	</section>
+
+</body>
+</html>
