ff570a3c15e08181682a87f1c14b7414f16dd22d
Azzouz <mohamed.azzouz@laplateforme.io>
2020-05-05 16:05:55 +0200

    JOB03

---
+++ jour02/job03/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Runtrack4/Jour02</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+	<header>
+		<nav class="blue darken-1 nav-wrapper">
+			<ul id=" nav-mobile" class=" right hide-on-med-and-down">
+				<li><a href="index.php">Accueil</a></li>
+				<li><a href="index.php">Inscription</a></li>
+				<li><a href="index.php">Connexion</a></li>
+				<li><a href="index.php">Rechercher</a></li>
+			</ul>
+		</nav>
+
+		 
+	</header>
+
+	<main>
+		<section>
+			<form>
+				<label>Mr.</label>
+				<input type="radio" name="">
+				<br />
+
+				<label>Mme.</label>
+				<input type="radio" name="">
+				<br />
+
+				<label>Prenom</label>
+				<input type="text" name="">
+				<br />
+
+				<label>Nom</label>
+				<input type="text" name="">
+				<br />
+
+				<label>Adresse</label>
+				<input type="text" name="">
+				<br />
+
+				<label>Email</label>
+				<input type="email" name="">
+				<br />
+
+				<label>Password</label>
+				<input type="password" name="">
+				<br />
+
+				<label>Confirmer Password</label>
+				<input type="password" name="">
+				<br />
+
+				<label>Informatique</label>
+				<input type="checkbox" name="">
+				<br />
+
+				<label>Voyages</label>
+				<input type="checkbox" name="">
+				<br />
+
+				<label>Sport</label>
+				<input type="checkbox" name="">
+				<br />
+
+				<label>Lecture</label>
+				<input type="checkbox" name="">
+				<br />
+				
+				<input type="submit" name="" value="Valider">
+
+				
+			</form>
+		</section>
+		
+	</main>
+
+	<footer class="blue darken-1 page-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col l6 s12">
+					<h5 class="white-text">Footer Content</h5>
+					<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+				</div>
+				
+				<ul class="col l4 offset-l2 s12">
+					<li><a class="grey-text text-lighten-3" href="index.php">Accueil</a></li>
+					<li><a class="grey-text text-lighten-3" href="index.php">Inscription</a></li>
+					<li><a class="grey-text text-lighten-3" href="index.php">Connexion</a></li>
+					<li><a class="grey-text text-lighten-3" href="index.php">Rechercher</a></li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+
+	
+
+
+</body>
+</html>
