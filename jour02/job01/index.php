732898e39cad1b6622c30da6e243dd4448058cc6
Azzouz <mohamed.azzouz@laplateforme.io>
2020-05-05 15:20:41 +0200

    JOB01

---
+++ jour02/job01/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Job01</title>
+	
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Accueil</a></li>
+				<li><a href="index.php">Inscription</a></li>
+				<li><a href="index.php">Connexion</a></li>
+				<li><a href="index.php">Rechercher</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<section>
+			<form>
+				<label>Mr.</label>
+				<input type="radio" name="">
+				<br />
+
+				<label>Mme.</label>
+				<input type="radio" name="">
+				<br />
+
+				<label>Prenom</label>
+				<input type="text" name="">
+				<br />
+
+				<label>Nom</label>
+				<input type="text" name="">
+				<br />
+
+				<label>Adresse</label>
+				<input type="text" name="">
+				<br />
+
+				<label>Email</label>
+				<input type="email" name="">
+				<br />
+
+				<label>Password</label>
+				<input type="password" name="">
+				<br />
+
+				<label>Confirmer Password</label>
+				<input type="password" name="">
+				<br />
+
+				<label>Informatique</label>
+				<input type="checkbox" name="">
+				<br />
+
+				<label>Voyages</label>
+				<input type="checkbox" name="">
+				<br />
+
+				<label>Sport</label>
+				<input type="checkbox" name="">
+				<br />
+
+				<label>Lecture</label>
+				<input type="checkbox" name="">
+				<br />
+				
+				<input type="submit" name="" value="Valider">
+
+				
+			</form>
+		</section>
+		
+	</main>
+
+	<footer>
+		<nav>
+			<ul>
+				<li><a href="index.php">Accueil</a></li>
+				<li><a href="index.php">Inscription</a></li>
+				<li><a href="index.php">Connexion</a></li>
+				<li><a href="index.php">Rechercher</a></li>
+			</ul>
+		</nav>
+	</footer>
+
+
+</body>
+</html>
