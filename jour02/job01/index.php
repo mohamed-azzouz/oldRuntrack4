78c208ef0c6e877345da784347334416fc94b296
Azzouz <mohamed.azzouz@laplateforme.io>
2020-05-05 15:55:55 +0200

    JOB01

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -76,14 +76,14 @@
 	</main>
 
 	<footer>
-		<nav>
-			<ul>
-				<li><a href="index.php">Accueil</a></li>
-				<li><a href="index.php">Inscription</a></li>
-				<li><a href="index.php">Connexion</a></li>
-				<li><a href="index.php">Rechercher</a></li>
-			</ul>
-		</nav>
+
+		<ul>
+			<li><a href="index.php">Accueil</a></li>
+			<li><a href="index.php">Inscription</a></li>
+			<li><a href="index.php">Connexion</a></li>
+			<li><a href="index.php">Rechercher</a></li>
+		</ul>
+		
 	</footer>
 
 
