1b681906622f38106420bdc08eb0dc79ae41690f
Azzouz <mohamed.azzouz@laplateforme.io>
2020-05-06 17:35:27 +0200

    job01

---
+++ jour03/job01/index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Runtrack4/Jour3</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body style="background:#dcdcdc">
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">LPTF</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse-show navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Units</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Jobs</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Skills</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+	
+	<main class="container-fluid">
+		
+			
+			<h1 class="text-center display-4">LaPlateforme_</h1>
+
+
+			<section class="row justify-content-around">
+				
+				<div class="card col-xs-12 col-md-4 col-xl-2" style="width: 18rem; height: 450px;">
+					<img src="papillon.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Un Papillon</h5>
+						<p class="card-text">Un papillon, c'est un peu comme une chenille, mais avec des ailes.<br>Ne pas ingerer.</p>
+						<a href="#" class="btn btn-primary text-center">Commander votre propre papillon</a>
+					</div>
+				</div>
+
+				<div class="jumbotron col-xl-8" style="width: 70rem;">
+					<h1 class="display-4">Bonjour, monde!</h1>
+					<p class="lead">Il existe plusieurs vision du terme:<br><br>le monde est la matière, l'espace et les phénomènes qui nous sont accessibles par les sens, l'expérience ou la raison.<br><br>Le sens le plus courant désigne notre planète, la Terre, avec ses habitants, et son environnement plus ou moins naturel.</p>
+					<hr class="my-4">
+					<p>Le sens étendu désigne l'univers dans son ensemble.</p>
+					<a class="btn btn-primary btn-lg" href="#" role="button" style="background-color: red">Rebooter le Monde</a>
+					<div class="spinner-border text-info" role="status">
+						<span class="sr-only">Loading...</span>
+					</div>
+
+					<nav aria-label="Page navigation example">
+						<ul class="pagination row justify-content-end">
+							<li class="page-item">
+								<a class="page-link" href="#" aria-label="Previous">
+									<span aria-hidden="true">&laquo;</span>
+								</a>
+							</li>
+							<li class="page-item"><a class="page-link" href="#">1</a></li>
+							<li class="page-item"><a class="page-link" href="#">2</a></li>
+							<li class="page-item"><a class="page-link" href="#">3</a></li>
+							<li class="page-item">
+								<a class="page-link" href="#" aria-label="Next">
+									<span aria-hidden="true">&raquo;</span>
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+
+				<div class="col-xl-1.5">
+
+					<ul class="list-group " style="width: 12rem;">
+						<li class="list-group-item active">Limbes</li>
+						<li class="list-group-item">Luxure</li>
+						<li class="list-group-item">Gourmandise</li>
+						<li class="list-group-item">Avarice</li>
+						<li class="list-group-item">Colere</li>
+						<li class="list-group-item">Heresie</li>
+						<li class="list-group-item">Violence</li>
+						<li class="list-group-item">Ruse et Tromperie</li>
+						<li class="list-group-item">Trahison</li>
+						<li class="list-group-item">Internet Explorer</li>
+					</ul>
+				</div>
+			</section>
+			<section  style="width: 65%; margin: 0 auto;">
+				
+				<p class="text-right font-weight-bold col-11">Installation de AI 9000</p>
+				<div class="row justify-content-center">
+					<svg class="bi bi-arrow-bar-left row col-xl-1" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 00-.708 0l-3 3a.5.5 0 000 .708l3 3a.5.5 0 00.708-.708L3.207 8l2.647-2.646a.5.5 0 000-.708z" clip-rule="evenodd" class="row justify-content-end"/>
+						<path fill-rule="evenodd" d="M10 8a.5.5 0 00-.5-.5H3a.5.5 0 000 1h6.5A.5.5 0 0010 8zm2.5 6a.5.5 0 01-.5-.5v-11a.5.5 0 011 0v11a.5.5 0 01-.5.5z" clip-rule="evenodd"/>
+					</svg>
+					<div class="progress container row col-xl-10">
+						
+						<div class="progress-bar progress-bar-striped bg-warning row just " role="progressbar" style="width: 90%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+						
+					</div>
+					<svg class="bi bi-arrow-bar-right col-xl-1 row" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" d="M10.146 4.646a.5.5 0 01.708 0l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L12.793 8l-2.647-2.646a.5.5 0 010-.708z" clip-rule="evenodd"/>
+						<path fill-rule="evenodd" d="M6 8a.5.5 0 01.5-.5H13a.5.5 0 010 1H6.5A.5.5 0 016 8zm-2.5 6a.5.5 0 01-.5-.5v-11a.5.5 0 011 0v11a.5.5 0 01-.5.5z" clip-rule="evenodd"/>
+					</svg>
+				</div>
+			</section>
+			<br>
+
+			<section class=" row justify-content-around">
+				<section class="col-xs-12 col-md-8 col-xl-4">
+					<label class="font-weight-bold">Recevez votre copie gratuite d'internet 2!</label>
+					<div class="input-group mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text" id="basic-addon1">@</span>
+						</div>
+						<input type="text" class="form-control" placeholder="Login" aria-label="Login" aria-describedby="basic-addon1">
+					</div>
+
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" placeholder="Mot de Passe" aria-label="Mot de Passe" aria-describedby="basic-addon2">
+						<div class="input-group-append">
+							<span class="input-group-text" id="basic-addon2">@example.com</span>
+						</div>
+					</div>
+
+					<label for="basic-url">URL des Internets 2 et 2.1 Beta</label>
+
+
+					<div class="input-group mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text">DogeCoin</span>
+						</div>
+						<input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+						<div class="input-group-append">
+							<span class="input-group-text">.00</span>
+						</div>
+					</div>
+
+					<div class="input-group mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text" id="basic-addon3">https://l33t.lptf/berlusconimkt/</span>
+						</div>
+						<input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+					</div>
+				</section>
+
+				<section class="col-xs-12 col-md-8  col-xl-4">
+					<form>
+						<div class="form-group">
+							<label for="exampleInputEmail1">Email address</label>
+							<input type="email" class="form-control col-11" id="exampleInputEmail1" aria-describedby="emailHelp">
+							<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+						</div>
+						<div class="form-group">
+							<label for="exampleInputPassword1">Password</label>
+							<input type="password" class="form-control col-11" id="exampleInputPassword1">
+						</div>
+						<div class="form-group form-check">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Check me out</label>
+						</div>
+						<button type="submit" class="btn btn-primary">Submit</button>
+					</form>
+				</section>
+			</section>
+		
+		
+		
+	</main>
+
+</body>
+</html>
